3176fd2d7b2f47006c1a45e50e524720e6f2ca9f
Jina Kim <jk6653284@1b58df6e-c74d-4ce8-99fc-146556260c05.fritz.box>
2022-01-06 13:17:44 +0100

    fix book id script bugs

--- src/review_scraper/get_bookids.py
+++ src/review_scraper/get_bookids.py
@@ -34,7 +34,7 @@ def setup_driver(headless=True):
     driver = webdriver.Firefox(options=fireFoxOptions)
     return driver
 
-def extract_book_isbn(file_path:str,shelf:str='read') -> list:
+def extract_book_isbn(file_name:str,shelf:str='read') -> list:
     """
     Read raw goodreas file and extract list of isbn
 
@@ -42,7 +42,8 @@ def extract_book_isbn(file_path:str,shelf:str='read') -> list:
     Output: list of isbn
     """
     # read file 
-    books_df = pd.read_csv(file_path)
+    filedir = os.path.join(os.path.dirname(__file__),f'input_data/{file_name}')
+    books_df = pd.read_csv(filedir)
     # filter out if shelf name is not empty
     if len(shelf) > 0 :
         books_df = books_df[books_df['Exclusive Shelf'] == shelf]
@@ -71,10 +72,8 @@ def open_url_from_isbn(driver,isbn:str,browser_url:str) -> None:
     search_box = driver.find_element(By.ID, 'sitesearch_field')
     if 'firefox' in driver.capabilities['browserName']:
         scroll_shim(driver, search_box)
-
     # initiate action chain 
     action = ActionChains(driver)
-
     # click on search bar
     action.move_to_element(search_box)
     action.click(search_box)
@@ -83,37 +82,51 @@ def open_url_from_isbn(driver,isbn:str,browser_url:str) -> None:
     # press enter key
     action.send_keys(Keys.ENTER)
     # perform action
+    action.pause(3)
     action.perform()
-    action.pause(2)
+
+def rertrieve_current_url(driver) -> str:
+    """
+    returns current url
+    """
+    return driver.current_url
     
 def main():
     search_url = "https://www.goodreads.com/"
 
     # parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('--book_list_path', type=str)
+    parser.add_argument('--book_list_file_name', type=str)
     parser.add_argument('--shelf_type', type=str, default='read')
-
     args = parser.parse_args()
+    print(f"Retrieving ISBNs from file {args.book_list_file_name}")
 
     # get ISBN 13 number
-    isbn_list = extract_book_isbn(file_path=args.book_list_path, shelf=args.shelf_type)
+    isbn_list = extract_book_isbn(file_name=args.book_list_file_name, shelf=args.shelf_type)
 
     # setup driver
     driver = setup_driver(headless=True)
 
     # get page url for each isbn
+    print(f"Started retrieving url for {len(isbn_list)} books...")
     urls = []
     for isbn in isbn_list:
         open_url_from_isbn(driver,isbn=str(isbn),browser_url=search_url)
-        urls.append(driver.current_url.split("show/")[-1])
+        url = rertrieve_current_url(driver)
+        urls.append(url.split("show/")[-1])
+        
     # close driver
+    print(f"Successfully retrieved urls for {len(urls)} books.")
     driver.quit()
     
     # save file with list of url as txt file
     dirname = os.path.dirname(__file__)
     output_pathname = os.path.join(dirname,'input_data/goodreads_mybooks.txt')
-    with open(output_pathname,'r') as f:
+    with open(output_pathname,'w') as f:
         for row in urls:
             f.write(row+"\n")
+    print(f"Output saved in {output_pathname}")
     
+if __name__ == '__main__':
+    main()
+
